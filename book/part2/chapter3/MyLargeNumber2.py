5e410b7400e62979b96efab51055313e05b39c46
preparedmale <preparedmale@gmail.com>
2021-08-03 17:39:32 +0900

    큰 수의 법칙 문제 실습2

---
+++ book/part2/chapter3/MyLargeNumber2.py
@@ -0,0 +1,23 @@
+# n = 입력받을 정수의 개수
+# m = 숫자를 더하는 총 횟수
+# k = 연속으로 더할수 있는 최대 횟수
+n, m, k = map(int, input().split())
+# 주어진 숫자 리스트
+data = list(map(int, input().split()))
+
+def Solution (n, m, k, data):
+    answer = 0
+
+    data.sort()
+    first_number = data.pop()
+    second_number = data.pop()
+
+    count = k * (k + 1 // m)
+    count += (count) % m
+
+    answer += count * first_number
+    answer += (m - count) * second_number
+
+    return answer
+
+print(Solution(n, m, k, data))
