a8bef933914b3aaf270683a609caf875c3fa6618
preparedmale@gmail.com <Vkffkdn12!>
2021-08-06 22:51:43 +0900

    시각 실습

--- book/part2/chapter3/Time/MyTime.py
+++ book/part2/chapter3/Time/MyTime.py
@@ -1,16 +1,13 @@
 # 정수 N을 입력받는다.
 n = int(input())
 
-
-
 def Solution(n):
     answer = 0
 
     for h in range(n + 1):
         for m in range(60):
             for s in range(60):
-                print(h, m, s)
-                if str(h).find('3') or str(m).find('3') or str(s).find('3'):
+                if str(h).find('3') != -1 or str(m).find('3') != -1 or str(s).find('3') != -1:
                     answer += 1
 
     return answer
