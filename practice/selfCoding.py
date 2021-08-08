bd655a96be2602504c1f6d7f88aa962fb7865288
preparedmale <preparedmale@gmail.com>
2021-08-08 22:21:21 +0900

    점프 투 파이썬 클래스 예제 실습

--- practice/selfCoding.py
+++ practice/selfCoding.py
@@ -1,8 +1,4 @@
-marks = [90, 25, 67, 45, 80]
-
-number = 0
-for mark in marks:
-    number = number + 1
-
-    if mark <= 60: continue
-    print("%d번 학생 축하합니다. 합격입니다." % number)
+import sys
+args = sys.argv[1:]
+for i in args:
+    print(i.upper(), end=" ")
